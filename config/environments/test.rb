5016af2b31bffb9ddd0bdd3760c9b92f9ef279fc
paul lahana <paul.lahana@gmail.com>
2020-08-31 11:30:22 +0200

    postgr-travis

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+  config.active_record.migration_error = false
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
